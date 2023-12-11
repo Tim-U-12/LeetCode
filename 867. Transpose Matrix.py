ac29dfeb54300b78f12ee7aece19fcff0bd701d1
Tim <tim.a.ung.12@gmail.com>
2023-12-11 11:23:59 +1100

    transpose matrix but only for square matrix

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -1,4 +1,11 @@
-class Solution:
-    def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
-        print("hello")
-        pass
+def transpose(matrix: list[list[int]]) -> list[list[int]]:
+    n = len(matrix)
+
+    for i in range(n):
+        for j in range(i):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    return matrix
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+transpose(matrix)
