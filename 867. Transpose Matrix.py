d8db3ccd7e90b46a80e430ef783e9efd02011a2d
Tim <tim.a.ung.12@gmail.com>
2023-12-12 15:28:38 +1100

    added comments

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -1,4 +1,8 @@
 def transpose(matrix: list[list[int]]) -> list[list[int]]:
+    '''
+        input: 2D matrix represented by list of list
+        output: returns a matrix where the row and column's indices have been switched
+    '''
     result = [[None] * len(matrix) for _ in range(len(matrix[0]))]
 
     for i in range(len(result)):
