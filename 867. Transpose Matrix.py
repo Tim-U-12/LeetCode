a043d8b2e43243c61a9e11d48b367af0b24229c2
Tim <tim.a.ung.12@gmail.com>
2023-12-12 15:23:46 +1100

    transpose function can now transpose matrices that have different rows and cols

--- 867. Transpose Matrix.py
+++ 867. Transpose Matrix.py
@@ -1,11 +1,11 @@
 def transpose(matrix: list[list[int]]) -> list[list[int]]:
-    n = len(matrix)
+    result = [[None] * len(matrix) for _ in range(len(matrix[0]))]
 
-    for i in range(n):
-        for j in range(i):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    for i in range(len(result)):
+        for j in range(len(result[0])):
+            result[i][j] = matrix[j][i]
 
-    return matrix
+    return result
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
-transpose(matrix)
+print(transpose(matrix))
