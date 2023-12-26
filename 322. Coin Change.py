00963018fc46eab9e0eb75d17ed3aea746ab8920
Tim <tim.a.ung.12@gmail.com>
2023-12-27 09:34:29 +1100

    create recursive coin change function

--- 322. Coin Change.py
+++ 322. Coin Change.py
@@ -5,27 +5,24 @@ class TestCoinChange(unittest.TestCase):
         self.assertEqual(coinChange([1,2,5], 11), 3)
 
     def testNone(self):
-        pass
+        self.assertEqual(coinChange([2], 3), -1)
 
     def testAmountNone(self):
-        pass
+        self.assertEqual(coinChange([1], 0), 0)
 
 def coinChange(coins: list[int], amount: int) -> int:
-    # Init
-    minCoins = [amount + 1] * (amount + 1)
-
+    minCoins = (amount + 1) * [float('inf')]
     minCoins[0] = 0
 
     for i in range(1, amount + 1):
         for coin in coins:
             if coin <= i:
-                minCoins[i] = min(minCoins[i], minCoins[i - coin] + 1)
-    
-    if minCoins[amount] > amount:
-        return - 1
-    
-    return minCoins[amount]
+                minCoins[i] = min(minCoins[i], minCoins[i-coin] + 1)
 
+    return -1 if minCoins[amount] > amount else minCoins[amount]
+
+def recursiveCoinChange(coins: list[int], amount: int) -> int:
+    pass
 
 
 if __name__ == "__main__":
