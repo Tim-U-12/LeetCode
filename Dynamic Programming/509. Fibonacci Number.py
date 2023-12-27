eb4f0efd356cb56abaa3353ab4239d667e95e7bb
Tim <tim.a.ung.12@gmail.com>
2023-12-27 12:21:37 +1100

    create bottom up fibonacci function

---
+++ Dynamic Programming/509. Fibonacci Number.py
@@ -0,0 +1,28 @@
+import unittest
+
+class testFibonacci(unittest.TestCase):
+    def testBottomUp(self):
+        self.assertEqual(bottomUpFibonacci(2), 1)
+        self.assertEqual(bottomUpFibonacci(3), 2)
+        self.assertEqual(bottomUpFibonacci(4), 3)
+    
+    def testTopDown(self):
+        pass
+
+def bottomUpFibonacci(n:int) -> int:
+    prev = 0
+    result = 1
+
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    for _ in range(n - 1):
+        prev, result = result, prev + result
+
+    return result
+
+
+if __name__ == "__main__":
+    unittest.main()
