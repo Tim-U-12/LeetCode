5a83c5cde58927adafce28e131a6e2234fa670da
Tim <tim.a.ung.12@gmail.com>
2023-12-27 12:27:07 +1100

    create top down fibonacci sequence

--- Dynamic Programming/509. Fibonacci Number.py
+++ Dynamic Programming/509. Fibonacci Number.py
@@ -7,7 +7,11 @@ class testFibonacci(unittest.TestCase):
         self.assertEqual(bottomUpFibonacci(4), 3)
     
     def testTopDown(self):
-        pass
+        self.assertEqual(topDownFibonacci(0), 0)
+        self.assertEqual(topDownFibonacci(1), 1)
+        self.assertEqual(topDownFibonacci(2), 1)
+        self.assertEqual(topDownFibonacci(3), 2)
+        self.assertEqual(topDownFibonacci(4), 3)
 
 def bottomUpFibonacci(n:int) -> int:
     prev = 0
@@ -22,6 +26,14 @@ def bottomUpFibonacci(n:int) -> int:
         prev, result = result, prev + result
 
     return result
+
+def topDownFibonacci(n:int) -> int:
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+    
+    return topDownFibonacci(n - 1) + topDownFibonacci(n - 2)
 
 
 if __name__ == "__main__":
