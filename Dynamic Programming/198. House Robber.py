89decacb97ffd1595ddfa8d15b7fc68f90d9f687
Tim <tim.a.ung.12@gmail.com>
2024-01-23 18:58:01 +1100

    init set up

---
+++ Dynamic Programming/198. House Robber.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestRob(unittest.TestCase):
+    def testRob(self):
+        pass
+
+def rob(nums: list(int)) -> int:
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
