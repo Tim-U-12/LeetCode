5658dc01605bc59c59d5a3b100dedcd50592938d
Tim <tim.a.ung.12@gmail.com>
2024-01-23 22:00:01 +1100

    complete

--- Dynamic Programming/198. House Robber.py
+++ Dynamic Programming/198. House Robber.py
@@ -2,10 +2,23 @@ import unittest
 
 class TestRob(unittest.TestCase):
     def testRob(self):
-        pass
+        self.assertEqual(rob([1,2,3,1]), 4)
+        self.assertEqual(rob([2,7,9,3,1]), 12)
+        self.assertEqual(rob([2,1,1,2]), 4)
+        self.assertEqual(rob([1,7,9,2]), 10)
 
-def rob(nums: list(int)) -> int:
-    pass
+def rob(nums: list[int]) -> int:
+    n = len(nums)
 
+    if n <= 2: return max(nums)
+    
+    temp = nums[0]
+    sol = max(nums[0], nums[1])
+
+    for i in range(2, n):
+        temp, sol = sol, max(sol, nums[i] + temp)
+
+    return sol   
+ 
 if __name__ == "__main__":
     unittest.main()
