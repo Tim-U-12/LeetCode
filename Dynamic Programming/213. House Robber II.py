a97c7e38776d90abeb6db3cd76fc21a82c4af4c4
Tim <tim.a.ung.12@gmail.com>
2024-01-29 12:38:40 +1100

    Create structure for House Robber 2

---
+++ Dynamic Programming/213. House Robber II.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestRob(unittest.TestCase):
+    def testGeneric(self):
+        pass
+
+
+def rob(nums: list[int]) -> int:
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
