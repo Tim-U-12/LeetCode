d8fdd8b4f8f4c8186c51fa9537290e7d863b7f67
Tim <tim.a.ung.12@gmail.com>
2024-01-29 13:35:55 +1100

    Solution to 213 House Robber 2 problem

--- Dynamic Programming/213. House Robber II.py
+++ Dynamic Programming/213. House Robber II.py
@@ -2,11 +2,29 @@ import unittest
 
 class TestRob(unittest.TestCase):
     def testGeneric(self):
-        pass
+        self.assertEqual(rob([2,3,2]), 3)
+        self.assertEqual(rob([1,2,3,1]), 4)
+        self.assertEqual(rob([1,2,3]), 3)
 
 
 def rob(nums: list[int]) -> int:
-    pass
+    n = len(nums)
+    if n <= 2:
+        return max(nums)
+    
+    temp = nums[0]
+    potential_sol_one = max(nums[0], nums[1])
 
+    for i in range(2, n - 1):
+        temp, potential_sol_one = potential_sol_one, max(temp + nums[i], potential_sol_one)
+    
+    temp_two = nums[1]
+    potential_sol_two = max(nums[1], nums[2])
+    for i in range(3, n):
+        temp, potential_sol_two = potential_sol_two, max(temp_two + nums[i], potential_sol_two)
+    
+    return max(potential_sol_two, potential_sol_one)
+
+    
 if __name__ == "__main__":
     unittest.main()
