00fa87bef68f40a35c6fb0be3616cf3ca2702f78
Tim <tim.a.ung.12@gmail.com>
2024-03-08 11:57:49 +1100

    Complete question using brute force method

--- 997. Find the Town Judge.py
+++ 997. Find the Town Judge.py
@@ -3,27 +3,28 @@ import unittest
 class TestFindJudge(unittest.TestCase):
     def testFindJudge(self) -> None:
         self.assertEqual(findJudge(3, [[1,3],[2,3]]), 3)
+        self.assertEqual(findJudge(3, [[1,3],[2,3],[3,1]]), -1)
 
 def findJudge(n:int, trust: list[list[int]]) -> int:
-    # The town judge trusts nobody.
-    suspects = [[True for _ in range(n)] for _ in range(n)]
-    
+    suspects = [[0 for _ in range(n)] for _ in range(n)]
+
     for t in trust:
-        print(t)
-        suspects[t[0] - 1][t[1] - 1] = False
-    
-    result = []
-    for i, row in enumerate(suspects):
-        if len(set(row)) == 1:
-            result.append(i)
-    
-    if len(result) == 1:
-        return result[0] + 1
+        suspects[t[0] - 1][t[1] - 1] = 1
+
+    print(suspects)
+
+    town_judge = []
+    for i in range(n):
+        count = 0
+        for j in range(n):
+            count += suspects[j][i]
+        if count == n - 1 and sum(suspects[i]) == 0:
+            town_judge.append(i + 1)
+
+    if len(town_judge) == 1:
+        return town_judge[0]
     else:
         return -1
-
-
-
-
+        
 if __name__ == "__main__":
     unittest.main()
