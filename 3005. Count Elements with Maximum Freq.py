6b2d1e3bdc3e1fad6b1b70870ec74858735b830b
MBP Tim <tim.a.ung.12@gmail.com>
2024-03-09 11:36:25 +1100

    complete initial solution

--- 3005. Count Elements with Maximum Freq.py
+++ 3005. Count Elements with Maximum Freq.py
@@ -2,18 +2,25 @@ import unittest
 
 class TestSolution(unittest.TestCase):
     def testSolution(self):
-        self.assertEqual(maxFrequencyElements([1,2,2,3,1,4]), 2)
+        self.assertEqual(maxFrequencyElements([1,2,2,3,1,4]), 4)
+        self.assertEqual(maxFrequencyElements([1,2,3,4,5]), 5)
 
 def maxFrequencyElements(nums: list[int]) -> int:
     myDict = {}
     maxFreq = 1
     for num in nums:
         if num in myDict:
-            maxFreq = max(maxFreq, myDict[num] + 1)
+            myDict[num] += 1
+            maxFreq = max(maxFreq, myDict[num])
         else:
             myDict[num] = 1 
 
-    return maxFreq
+    result = 0
+    for key in myDict:
+        if myDict[key] == maxFreq:
+            result += maxFreq
+        
+    return result
 
 if __name__ == "__main__":
     unittest.main()
