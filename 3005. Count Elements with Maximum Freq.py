cff9915fad02241e262595dd65f736b371267aec
MBP Tim <tim.a.ung.12@gmail.com>
2024-03-09 11:28:46 +1100

    calculates the max frequency

--- 3005. Count Elements with Maximum Freq.py
+++ 3005. Count Elements with Maximum Freq.py
@@ -1,2 +1,19 @@
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def testSolution(self):
+        self.assertEqual(maxFrequencyElements([1,2,2,3,1,4]), 2)
+
+def maxFrequencyElements(nums: list[int]) -> int:
+    myDict = {}
+    maxFreq = 1
+    for num in nums:
+        if num in myDict:
+            maxFreq = max(maxFreq, myDict[num] + 1)
+        else:
+            myDict[num] = 1 
+
+    return maxFreq
+
 if __name__ == "__main__":
-    print("hello world")
+    unittest.main()
