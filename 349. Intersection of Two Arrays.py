02ef07a4bab77468115df8e7ead533a0bb0bf476
MBP Tim <tim.a.ung.12@gmail.com>
2024-03-10 13:06:09 +1100

    set up unittest

--- 349. Intersection of Two Arrays.py
+++ 349. Intersection of Two Arrays.py
@@ -1,4 +1,8 @@
 import unittest
 
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        print("Hello World!")
+
 if __name__ == "__main__":
-    print("Hello World!")
+    unittest.main()
