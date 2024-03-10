11a4929dbaad500620a60aa443b55f91a3362e17
MBP Tim <tim.a.ung.12@gmail.com>
2024-03-10 13:28:30 +1100

    complete solution

--- 349. Intersection of Two Arrays.py
+++ 349. Intersection of Two Arrays.py
@@ -2,7 +2,30 @@ import unittest
 
 class TestSolution(unittest.TestCase):
     def test_solution(self):
-        print("Hello World!")
+        nums1 = [1,2,2,1]
+        nums2 = [2,2]
+        self.assertEqual(intersection(nums1, nums2), [2])
+
+        nums1 = [4,9,5]
+        nums2 = [9,4,9,8,4]
+        self.assertEqual(intersection(nums1, nums2), [9, 4])
+
+
+def intersection(nums1: list[int], nums2: list[int]) -> list[int]:
+    nums1 = set(nums1)
+    nums2 = set(nums2)
+
+    if len(nums1) < len(nums2):
+        return search(nums1, nums2)
+    return search(nums2, nums1)
+
+def search(nums1, nums2):
+    sol = []
+    for num in nums1:
+        if num in nums2:
+                sol.append(num)
+    return sol
+        
 
 if __name__ == "__main__":
     unittest.main()
