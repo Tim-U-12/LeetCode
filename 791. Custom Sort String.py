328b1afee1aa9c78d68f2ff565ea2c4af668d6af
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-03-11 13:16:20 +1100

    solve Question 791

--- 791. Custom Sort String.py
+++ 791. Custom Sort String.py
@@ -6,7 +6,25 @@ class TestSolution(unittest.TestCase):
         self.assertEqual(customSortString("bcafg", "abcd"), "bcad")
 
 def customSortString(order: str, s: str) -> str:
-    print("Hello World")
+    sol = ""
+    myDict = {}
+    for letter in s:
+        if letter in myDict:
+            myDict[letter] += 1
+        else:
+            myDict[letter] = 1
+    
+    for letter in order:
+        if letter in myDict:
+            sol += (myDict[letter] * letter)
+            del myDict[letter]
+    
+    for key in myDict:
+        sol += (myDict[key] * key)
+    
+    print(sol)
+    return sol
+
 
 
 if __name__ == "__main__":
