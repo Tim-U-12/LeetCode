600883936af93768e178ce6422645306bc68532b
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-03-14 10:46:40 +1100

    init pivotInteger

---
+++ Question 2485.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def test_sol(self):
+        self.assertEqual()
+
+def pivotInteger(n: int) -> int:
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
