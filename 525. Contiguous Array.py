2b5aaf8e6f013c4ac3cae7f7c9f1ef3a89de7a22
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-03-16 14:25:10 +1100

    set up testcases and function

---
+++ 525. Contiguous Array.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def test_sol(self):
+        pass
+
+def findMaxLength(nums: list[int]) -> int:
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
