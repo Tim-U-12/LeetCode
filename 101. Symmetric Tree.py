92c3fc3f3b357e6c93fb63860b46b6b257b1a69a
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-03-21 18:36:35 +1100

    solve

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -1,5 +1,24 @@
 import unittest
 
+class TreeNode():
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = self.left
+        self.right = right
+
+def isSymmetric(root) -> bool:
+    if not root:
+        return True
+    
+    def isMirror(t1, t2):
+        if  not t1 or not t2:
+            return t1 == t2
+        if t1.val != t2.val:
+            return False
+        
+        return isMirror(t1.left, t2.right) and isMirror(t1.left, t2.right)
+    return isMirror(root.left, root.right)
+
 class TestSolution(unittest.main()):
     def test_sol(self):
         pass
