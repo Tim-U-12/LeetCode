611ddfaa4dee11f5a01e70a845b924e09f2c66b7
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-03-23 15:40:09 +1100

    init

---
+++ 143. Reorder List.py
@@ -0,0 +1,17 @@
+import unittest
+
+class ListNode:
+    def __init__(self, val=0, next=None) -> None:
+        self.val = val
+        self.next = next
+
+def reorderList(head) -> None:
+    pass
+
+class TestSolution(unittest.TestCase):
+    def test_sol(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
+
