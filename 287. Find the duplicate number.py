c0dfa387ec92536d5e5779e19a6ee23ce44b5176
MBP Tim <tim.a.ung.12@gmail.com>
2024-03-24 11:50:13 +1100

    Solution exceeds time constraint

--- 287. Find the duplicate number.py
+++ 287. Find the duplicate number.py
@@ -2,9 +2,12 @@ import unittest
 
 class TestSolution(unittest.TestCase):
     def test_sol(self):
-        self.assertEqual(findDuplicates([1,3,4,2,2]), [1, 2, 2, 3, 4])
+        self.assertEqual(findDuplicates([1,3,4,2,2]), 2)
+        #self.assertEqual(findDuplicates([3,1,3,4,2]), 3)
+        #self.assertEqual(findDuplicates([3,3,3,3,3]), 3)
 
-def findDuplicates(nums: list[int]):
+
+def findDuplicates(nums: list[int]) -> int:
     n = len(nums)
     sorted_index = 0
 
@@ -13,10 +16,14 @@ def findDuplicates(nums: list[int]):
         for j in range(i, n):
             if nums[j] < nums[smallest_val_index]:
                 smallest_val_index = j
+        
+        if sorted_index > 0 and nums[sorted_index - 1] == nums[smallest_val_index]:
+            return nums[sorted_index - 1]
+        
         nums[sorted_index], nums[smallest_val_index] = nums[smallest_val_index], nums[sorted_index]
         sorted_index += 1
 
-    return nums
+    return -1
 
 if __name__ == "__main__":
     unittest.main()
