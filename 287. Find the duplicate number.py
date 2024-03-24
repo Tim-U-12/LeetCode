27773aef143d9c92055300234322b7c046801ded
MBP Tim <tim.a.ung.12@gmail.com>
2024-03-24 13:07:52 +1100

    tortoise and the hare approach

--- 287. Find the duplicate number.py
+++ 287. Find the duplicate number.py
@@ -3,27 +3,25 @@ import unittest
 class TestSolution(unittest.TestCase):
     def test_sol(self):
         self.assertEqual(findDuplicates([1,3,4,2,2]), 2)
-        #self.assertEqual(findDuplicates([3,1,3,4,2]), 3)
-        #self.assertEqual(findDuplicates([3,3,3,3,3]), 3)
+        self.assertEqual(findDuplicates([3,1,3,4,2]), 3)
+        self.assertEqual(findDuplicates([3,3,3,3,3]), 3)
 
 
 def findDuplicates(nums: list[int]) -> int:
-    n = len(nums)
-    sorted_index = 0
+    tortoise = nums[0]
+    hare = nums[0]
+    while True:
+        tortoise = nums[tortoise]
+        hare = nums[nums[hare]]
+        if tortoise == hare:
+            break
 
-    for i in range(n):
-        smallest_val_index = i
-        for j in range(i, n):
-            if nums[j] < nums[smallest_val_index]:
-                smallest_val_index = j
-        
-        if sorted_index > 0 and nums[sorted_index - 1] == nums[smallest_val_index]:
-            return nums[sorted_index - 1]
-        
-        nums[sorted_index], nums[smallest_val_index] = nums[smallest_val_index], nums[sorted_index]
-        sorted_index += 1
+    tortoise = nums[0]
+    while tortoise != hare:
+        tortoise = nums[tortoise]
+        hare = nums[hare]
 
-    return -1
+    return hare
 
 if __name__ == "__main__":
     unittest.main()
