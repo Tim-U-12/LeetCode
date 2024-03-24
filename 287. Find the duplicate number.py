cab1a153bbe7be35e1d44a4a567af288cc042afd
MBP Tim <tim.a.ung.12@gmail.com>
2024-03-24 11:20:12 +1100

    init setup

---
+++ 287. Find the duplicate number.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def test_sol(self):
+        pass
+
+def findDuplicates(nums: list[int]) -> int:
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
