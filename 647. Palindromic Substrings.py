0eb3b2050914c9bb8c8b421551888b5807661d8a
Tim-U-12 <tim.a.ung.12@gmail.com>
2024-03-25 11:50:09 +1100

    starting code

---
+++ 647. Palindromic Substrings.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestSolution(unittest.TestCase):
+    def test_sol(self):
+        pass
+
+def countSubstrings(s: str):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
